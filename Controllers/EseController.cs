5e44e6326c85dd3028448b93e8922340c2db77a8
HanB2 <35714166+HanB2@users.noreply.github.com>
2018-06-28 14:14:08 +0900

    Add files via upload

---
+++ Controllers/EseController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using web_esm.Models;
+
+namespace web_esm.Controllers
+{
+    public class EseController : Controller
+    {
+		FilterSessionModels chk = new FilterSessionModels();
+
+		// GET: Ese SENDER ESE 정보 관리
+		public ActionResult EseInfo(string searchString, string msg)
+        {
+			//권한 체크===================================================
+			if (!chk.chkPermission("EseInfo", "PER_SELECT"))
+				return RedirectToAction("Index", "Home", new { msg = chk.alertStr });
+			//===========================================================
+
+			TempData["PublicMsg"] = null;
+			if (!string.IsNullOrEmpty(msg))
+				TempData["PublicMsg"] = msg;
+
+			return View();
+        }
+    }
+}
